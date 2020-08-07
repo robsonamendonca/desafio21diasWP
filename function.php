ab8d3b3247141f7c80dda439c017fe54bab857c0
Robson Mendonça <rob.ams@gmail.com>
2020-08-07 06:18:09 -0300

    Estrutura tema wordpress!

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Tema construido no Desafio21Dias
+ *
+ * @link http://torneseumprogramador.com.br
+ *
+ * @package WordPress
+ * @subpackage Desafio21Dias
+ * @since Desafio21Dias
+ */
