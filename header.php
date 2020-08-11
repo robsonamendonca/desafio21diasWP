3167ad7524df072865c6ae1180d4003babc02413
Robson Mendonça <rob.ams@gmail.com>
2020-08-11 07:49:21 -0300

    Ajuste no modal, listando post com limit de 3 por pagina(WP_Query).

--- header.php
+++ header.php
@@ -15,77 +15,99 @@ $url = get_stylesheet_directory_uri();
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Um tema criado por alunos do Torne-se um Programador" />
-        <meta name="author" content="Alunos Torne-se um Programador" />
-        <meta name='ir-site-verification-token' value='-28223945'>
 
-        <meta property="og:title" content="Freelancer - Theme Preview">
-        <meta property="og:site_name" content="Robson Mendonça">
-        <meta property="og:type" content="website">
-        <meta property="og:description" content="">
-        <meta property="og:image" content="<?php echo $url;?>/assets/img/branding/og-start-bootstrap.png">
-        <meta property="og:url" content="<?php echo $url;?>/previews/freelancer/">
-        <meta property="og:image:alt" content="Freelancer - Theme Preview">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Um tema criado por alunos do Torne-se um Programador" />
+    <meta name="author" content="Alunos Torne-se um Programador" />
+    <meta name='ir-site-verification-token' value='-28223945'>
 
-        <meta name="twitter:card" content="summary">
-        <meta name="twitter:image" content="<?php echo $url;?>/assets/img/branding/start-bootstrap-logo-500x500.png">
-        <meta name="twitter:site" content="@SBootstrap">
+    <meta property="og:title" content="Freelancer - Theme Preview">
+    <meta property="og:site_name" content="Robson Mendonça">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="<?php echo $url;?>/assets/img/branding/og-start-bootstrap.png">
+    <meta property="og:url" content="<?php echo $url;?>/previews/freelancer/">
+    <meta property="og:image:alt" content="Freelancer - Theme Preview">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $url;?>/assets/img/icons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $url;?>/assets/img/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $url;?>/assets/img/icons/favicon-16x16.png">
-        <link rel="manifest" href="<?php echo $url;?>/assets/img/icons/site.webmanifest">
-        <link rel="mask-icon" href="<?php echo $url;?>/assets/img/icons/safari-pinned-tab.svg" color="#dd3d31">
-        <meta name="msapplication-TileColor" content="#dd3d31">
-        <meta name="theme-color" content="#ffffff">     
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:image" content="<?php echo $url;?>/assets/img/branding/start-bootstrap-logo-500x500.png">
+    <meta name="twitter:site" content="@SBootstrap">
+    <!-- icons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $url;?>/assets/img/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $url;?>/assets/img/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $url;?>/assets/img/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $url;?>/assets/img/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $url;?>/assets/img/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $url;?>/assets/img/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $url;?>/assets/img/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $url;?>/assets/img/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $url;?>/assets/img/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo $url;?>/assets/img/icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $url;?>/assets/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $url;?>/assets/img/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $url;?>/assets/img/vfavicon-16x16.png">
+    <link rel="manifest" href="<?php echo $url;?>/assets/img/icons/manifest.json">
 
-        <title>Freelancer - Robson Mendonça</title>        
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="<?php echo $url;?>/assets/img/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="<?php echo $url;?>/assets/css/styles.css" rel="stylesheet" />
-    </head>
-    <body id="page-top">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Robson Mendonça</a>
-                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Sobre</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contato</a></li>
-                    </ul>
-                </div>
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="msapplication-TileColor" content="#dd3d31">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>Freelancer - Robson Mendonça</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="<?php echo $url;?>/assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
+        type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="<?php echo $url;?>/assets/css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Robson Mendonça</a>
+            <button
+                class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
+                type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
+                aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                            href="#portfolio">Habilidades</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                            href="#about">Sobre</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                            href="#contact">Contato</a></li>
+                </ul>
             </div>
-        </nav>
-        <!-- Masthead-->
-        <header class="masthead bg-primary text-white text-center">
-            <div class="container d-flex align-items-center flex-column">
-                <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="<?php echo $url;?>/assets/img/robson.png" alt="robsonamendonca" />
-                <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">Robson Mendonça</h1>
-                <!-- Icon Divider-->
-                <div class="divider-custom divider-light">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">Desenvolvedor Web, Consultor e Voluntário em São Paulo, Brasil</p>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead bg-primary text-white text-center">
+        <div class="container d-flex align-items-center flex-column">
+            <!-- Masthead Avatar Image-->
+            <img class="masthead-avatar mb-5" src="<?php echo $url;?>/assets/img/robson.png" alt="robsonamendonca" />
+            <!-- Masthead Heading-->
+            <h1 class="masthead-heading text-uppercase mb-0">Robson Mendonça</h1>
+            <!-- Icon Divider-->
+            <div class="divider-custom divider-light">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
             </div>
-        </header>
-
+            <!-- Masthead Subheading-->
+            <p class="masthead-subheading font-weight-light mb-0">Desenvolvedor Web, Consultor e Voluntário em São
+                Paulo, Brasil</p>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -24,70 +24,102 @@
             <div class="divider-custom-line"></div>
         </div>
         <div class="row">
-        <!-- Portfolio Grid Items-->
-        <?php
-                    if( have_posts()){
-                        $i=1;
-                        while ( have_posts()){
-                            the_post(); 
-                            $portfolioId= "portfolioModal".$i;                            
-                     ?>
+            <!-- Portfolio Grid Items-->
+            <?php
+        $args = array(
+            'posts_per_page'=> 3,
+            'post_type'=> 'post'
+        );
+        $the_query_post = new WP_Query($args);
+        if($the_query_post->have_posts()){           
+            $i=1;
+            // while ($the_query_post->have_posts()) {                
+            //     $the_query_post->the_post();   
+            foreach ($the_query_post->posts as $key => $post) {
+                # code...
+          
+                $portfolioId= "portfolioModal".$i;                            
+            ?>
 
             <!-- Portfolio Item 1-->
             <div class="col-md-6 col-lg-4 mb-5">
-                <div class="portfolio-item mx-auto" data-toggle="modal" data-target=<?php echo "'#{$portfolioId}'"?> >
+                <div class="portfolio-item mx-auto" data-toggle="modal" data-target=<?php echo "'#{$portfolioId}'"?>>
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white">
-                         <?php echo the_title();?>
+
+                            <!-- <?php //the_title();?> -->
+                            <?php 
+                                echo "Titulo: ".$post->post_title."<br>";  
+                                echo "Nome: ".$post->post_name."<br>";
+                                echo "Tipo: ".$post->post_type."<br>";
+                                echo "Alterado: ".$post->post_modified."<br>";
+                                echo "Status: ".$post->post_status."<br>";                                                      
+                            ?>
                         </div>
-                    </div>                    
+                    </div>
                     <div class="row">
-                     <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid', 'style'=>'width:100%')); ?>
+                        <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid', 'style'=>'width:100%')); ?>
                     </div>
                 </div>
             </div>
             <!-- fim Portfolio Item 1-->
-        <!-- Portfolio Modals-->
-        <!-- Portfolio Modal 1-->
-        <div class="portfolio-modal modal fade" id=<?php echo "'{$portfolioId}'"?> tabindex="-1" role="dialog" aria-labelledby="portfolioModal1Label" aria-hidden="true">
-            <div class="modal-dialog modal-xl" role="document">
-                <div class="modal-content">
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true"><i class="fas fa-times"></i></span>
-                    </button>
-                    <div class="modal-body text-center">
-                        <div class="container">
-                            <div class="row justify-content-center">
-                                <div class="col-lg-8">
-                                    <!-- Portfolio Modal - Title-->
-                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0" id="portfolioModal1Label"><?php echo the_title();?></h2>
-                                    <!-- Icon Divider-->
-                                    <div class="divider-custom">
-                                        <div class="divider-custom-line"></div>
-                                        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                                        <div class="divider-custom-line"></div>
+            <!-- Portfolio Modals-->
+            <!-- Portfolio Modal 1-->
+            <div class="portfolio-modal modal fade" id=<?php echo "'{$portfolioId}'"?> tabindex="-1" role="dialog"
+                aria-labelledby="portfolioModal1Label" aria-hidden="true">
+                <div class="modal-dialog modal-xl" role="document">
+                    <div class="modal-content">
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true"><i class="fas fa-times"></i></span>
+                        </button>
+                        <div class="modal-body text-center">
+                            <div class="container">
+                                <div class="row justify-content-center">
+                                    <div class="col-lg-8">
+                                        <!-- Portfolio Modal - Title-->
+                                        <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0"
+                                            id="portfolioModal1Label">
+                                            <?php //the_title();
+                                          echo $post->post_title;
+                                     ?>
+                                        </h2>
+                                        <!-- Icon Divider-->
+                                        <div class="divider-custom">
+                                            <div class="divider-custom-line"></div>
+                                            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                            <div class="divider-custom-line"></div>
+                                        </div>
+                                        <!-- Portfolio Modal - Image-->
+                                        <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid rounded mb-5')); ?>
+                                        <!-- Portfolio Modal - Text-->
+                                        <p class="mb-5">
+                                            <?php //the_content(); 
+                                           echo $post->post_content;
+                                        ?>
+                                        </p>
+                                        <button class="btn btn-primary" data-dismiss="modal">
+                                            <i class="fas fa-times fa-fw"></i>
+                                            Fechar Janela
+                                        </button>
                                     </div>
-                                    <!-- Portfolio Modal - Image-->
-                                    <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid rounded mb-5')); ?>
-                                    <!-- Portfolio Modal - Text-->
-                                    <p class="mb-5">O jogo da velha ou jogo do galo ou três em linha é um jogo e/ou passatempo popular. É um jogo de regras extremamente simples, que não traz grandes dificuldades para seus jogadores e é facilmente aprendido. Wikipédia.</p>
-                                    <button class="btn btn-primary" data-dismiss="modal">
-                                        <i class="fas fa-times fa-fw"></i>
-                                        Fechar Janela
-                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- fim Portfolio Modal 1-->
-        <?php           
-                          $i++;
-                        }// end while
-                    }//end if
-                    ?>
+            <!-- fim Portfolio Modal 1-->
+            <?php           
+                $i++;
+            }// end while
+        }//
+        else{ ?>
+            <div class="text-center mt-4 alert alert-danger" role="alert">
+                <p >Nenhum post encontrado!</p>
+            </div>
+            <?php           
+        }//end if
+        ?>
         </div>
     </div>
 
@@ -107,17 +139,20 @@
         <div class="row">
             <div class="col-lg-4 ml-auto">
                 <p class="lead">Ei, eu sou o Robson. Eu sou um desenvolvedor web que mora em São Paulo, Brasil.
-                 Sou fã de tecnologia, café e programação. Eu também estou interessado em desenvolvimento web e voluntariado.
-                 </p>
+                    Sou fã de tecnologia, café e programação. Eu também estou interessado em desenvolvimento web e
+                    voluntariado.
+                </p>
             </div>
             <div class="col-lg-4 mr-auto">
-                <p class="lead">Hey there, I’m Robson. I’m a web developer living in São Paulo, Brasil. 
-                I am a fan of technology, coffee, and programming. I’m also interested in web development and volunteering.</p>
+                <p class="lead">Hey there, I’m Robson. I’m a web developer living in São Paulo, Brasil.
+                    I am a fan of technology, coffee, and programming. I’m also interested in web development and
+                    volunteering.</p>
             </div>
         </div>
         <!-- About Section Button-->
         <div class="text-center mt-4">
-            <a class="btn btn-xl btn-outline-light" href="https://robsonamendonca.github.io/jogodavelha/index.html" target="_blank">
+            <a class="btn btn-xl btn-outline-light" href="https://robsonamendonca.github.io/jogodavelha/index.html"
+                target="_blank">
                 <i class="fas fa-gamepad mr-2"></i>
                 Vamos Jogar!
             </a>
