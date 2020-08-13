f6a8855c91edfbca1694981db7fc10eda4cd22ae
Robson Mendonça <rob.ams@gmail.com>
2020-08-13 09:42:39 -0300

    Adicionado paginação, estilo da exibição e personalizar itens por pagina.

--- page.php
+++ page.php
@@ -9,21 +9,38 @@
  * @since Desafio21Dias
  */
 
- get_header();
- $url = get_stylesheet_directory_uri();
+get_header();
 
+$posts_per_page = isset($_POST["per_page"]) ? $_POST["per_page"] : 2;
+$url = get_stylesheet_directory_uri();
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $args = array(
-    'posts_per_page'=> 30,
-    'post_type'=> 'post'
+    'posts_per_page'=> $posts_per_page,
+    'paged'  => $paged,
+    'post_type'     => 'post'
 );
 $the_query_post = new WP_Query($args);
 if($the_query_post->have_posts()){  ?>
 <div class="container" style="margin-top: 150px;">
     <h2>Habilidades</h2>
-    <p>Digite algo no campo abaixo para pesquisar títulos ou conteúdos na tabela:</p>
+    <p>Itens por páginas: <?php echo $posts_per_page; ?></p>
+    <form name="frmper_page" action="" method="post">
+        <select class="custom-select d-block w-20" name="per_page" id="per_page" placeholder="Itens por pagina..."
+            required="">
+            <option value="2">Escolha</option>
+            <option value="2">2</option>
+            <option value="4">4</option>
+            <option value="10">10</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+            <option value="500">500</option>
+        </select>
+    </form>
+    <hr/>
+    <p>Digite algo no campo abaixo para pesquisar títulos ou conteúdos na página corrente:</p>
     <input class="form-control" id="txtbusca" type="text" placeholder="Pesquisa...">
     <br>
-    <table class="table" id="tabHabilidades" >
+    <table class="table" id="tabHabilidades">
         <thead>
             <tr>
                 <th scope="col">#</th>
@@ -40,7 +57,11 @@ if($the_query_post->have_posts()){  ?>
                         ?>
             <tr>
                 <th scope="row"><?php the_id();?></th>
-                <td><?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid', 'style'=>'width:100%'));?></td>
+                <td>
+                    <div style="width:128px; height: 128px; overflow: hidden;">
+                        <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid', 'style'=>'width:100%'));?>
+                    </div>
+                </td>
                 <td><?php the_title();?></td>
                 <td><?php the_content();?></td>
             </tr>
@@ -50,6 +71,18 @@ if($the_query_post->have_posts()){  ?>
                         ?>
 
         </tbody>
+        <caption>
+            <?php
+        global $wp_query; 
+        $wp_query = $the_query_post;
+        echo paginate_links(array(
+            'prev_text' => __('<i class="fa fa-reply-all" aria-hidden="true"></i>'),
+            'next_text' => __('<i class="fa fa-share" aria-hidden="true"></i>'),
+            'before_page_number' => ' [ ',       
+            'after_page_number'  => ' ] ',
+        ));
+?>
+        </caption>
     </table>
 </div>
 <?php
@@ -63,17 +96,21 @@ else{ ?>
 ?>
 </div>
 </div>
-
 </section>
 
 <?php get_footer();?>
 <script>
-$(document).ready(function(){
-  $("#txtbusca").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#tabHabilidades tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+$(document).ready(function() {
+    $("#txtbusca").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#tabHabilidades tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
-  });
+
+    $('#per_page').on('change', function() {
+        document.forms['frmper_page'].submit();
+    });
+
 });
 </script>
